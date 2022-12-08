f9d33a89e7f3e1cb387209998afc975c89a174a7
nzouda777 <nzoudamartial@gmail.com>
2022-12-08 01:46:18 +0100

    maj

---
+++ card-slider/app.js
@@ -0,0 +1,37 @@
+
+    (function ($) {
+      $(function () {
+    
+      var agSwiper = $('.mySwiper');
+    
+      if (agSwiper.length > 1) {
+    
+        var sliderView = 1;
+        var ww = $(window).width();
+        if (ww >= 1060) sliderView = 4;
+        if (ww <= 800) sliderView = 3;
+        if (ww <= 769) sliderView = 2;
+        if (ww <= 400) sliderView = 1;
+    
+        var swiper = new Swiper('.mySwiper', {
+          slidesPerView: sliderView,
+          spaceBetween: 30,
+          loop: true,
+          speed: 700,
+        });
+    
+        $(window).resize(function () {
+          var ww = $(window).width();
+          if (ww >= 1060) swiper.params.slidesPerView = 4;
+          if (ww <= 800) swiper.params.slidesPerView = 3;
+          if (ww <= 769) swiper.params.slidesPerView = 2;
+          if (ww <= 400) swiper.params.slidesPerView = 1;
+        });
+    
+        $(window).trigger('resize');
+    
+        
+      }
+    
+      });
+    })(jQuery);
