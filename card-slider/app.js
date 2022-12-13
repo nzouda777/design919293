f9082a5e4867af551125d0f0ab1f5ab84cb8b5be
nzouda777 <nzoudamartial@gmail.com>
2022-12-13 03:02:45 +0100

    maj

--- card-slider/app.js
+++ card-slider/app.js
@@ -9,8 +9,8 @@
         var sliderView = 1;
         var ww = $(window).width();
         if (ww >= 1060) sliderView = 4;
-        if (ww <= 800) sliderView = 3;
-        if (ww <= 769) sliderView = 2;
+        if (ww <= 960) sliderView = 2 || 3;
+        if (ww <= 769) sliderView = 1;
         if (ww <= 400) sliderView = 1;
     
         var swiper = new Swiper('.mySwiper', {
@@ -27,8 +27,8 @@
         $(window).resize(function () {
           var ww = $(window).width();
           if (ww >= 1060) swiper.params.slidesPerView = 4;
-          if (ww <= 800) swiper.params.slidesPerView = 3;
-          if (ww <= 769) swiper.params.slidesPerView = 2;
+          if (ww <= 960) swiper.params.slidesPerView = 3;
+          if (ww <= 769) swiper.params.slidesPerView = 1;
           if (ww <= 400) swiper.params.slidesPerView = 1;
         });
     
--- mas/app.js
+++ mas/app.js
@@ -1,13 +1,27 @@
 
 
-let card = $(".card-img")
-console.log(card)
+let card = $("#pop1")
 let modal = $("#modal")
-console.log(modal)
 card.click(() => {
     modal.css({"display": "block"})
 })
-
-$(".closeModal").click(() => {
-    modal.css({"display": "none"})
+let closebtn = $(".closeModal")
+closebtn.click(() => {
+    modal.hide()
 })
+
+modal.click(() => {
+    console.log(modal)
+    modal.hide()
+})
+
+$(function () {
+    $(".content").slice(0, 6).show();
+    $("body").on('click touchstart', '.show-more-btn', function (e) {
+        e.preventDefault();
+        $(".content:hidden").slice(0, 6).fadeIn();
+        if ($(".content:hidden").length == 0) {
+            $(".show-more-btn").css('visibility', 'hidden');
+        }
+    });
+});
