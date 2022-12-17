6ec8c3945e7eccd3f3226da5751c9b2cc9ad0888
nzouda777 <nzoudamartial@gmail.com>
2022-12-17 02:30:46 +0100

    header

---
+++ slide-3-section/app.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 250000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
