4bf01bfecede44925a5bd988f063605aaeeea64b
nzouda777 <nzoudamartial@gmail.com>
2022-12-20 12:56:45 +0100

    maj

--- br-card-ms/app.js
+++ br-card-ms/app.js
@@ -1,14 +1,11 @@
 
-    // let dText = $('.br-ms-marquee-text').attr('data-text')
-    // $('.br-ms-marquee-text').text(dText)
-
     $(document).on('load',()=>{
         let dText = $('.bs-ms-marquee-text').val()
         $('.bs-ms-marquee-text').text(dText)
         
     })
 $(window).scroll( () => {
-            $('.bs-left-text-el').css("position", "fixed")
+            $('.bs-left-text-el').css({"position": "sticky", "top" :"45%", "bottom": "auto"})
         })
 $(window).scroll( () => {
     $('.bs-right-text-el').css("position", "fixed")
--- slide-3-section/app.js
+++ slide-3-section/app.js
@@ -18,8 +18,17 @@ var swiper = new Swiper(".mySwiper", {
 
   $(".closebtn").on('click', () => {
 
-    $(".bs-mobile-nav").css({"transform": "translateX(-100%)"})
+    $(".br-mobile-nav").css({"transform": "translateX(-100%)"})
 })
-$("#bs-mobile-btn-nav").on('click', () => {
-    $(".bs-mobile-nav").css({"transform": "translateX(0%)"})
+$("#br-mobile-btn-nav").on('click', () => {
+    $(".br-mobile-nav").css({ "visibility": "visible","transform": "translateX(0%)"})
+})
+$(window).scroll(() => {
+  let $height = $(window).scrollTop()
+  if ($height > 30) {
+    $(".br-nav-navbar").css({"transition" : "all 20s", "position":"fixed", "width":"100%","z-index":"3", "background": "url('https://global-uploads.webflow.com/600fe7d75184f7b68f55c88e/623da4c89a3f8d677f827868_header-back.png')"})
+  }else{
+    $(".br-nav-navbar").css({"transition" : "all 2s","position":"relative", "width":"100%","z-index":"3", "background": "transparent"})
+  }
+  
 })
