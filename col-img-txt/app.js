d99ed8d552a856ed53956d7fc183a0a0b56efcda
nzouda777 <nzoudamartial@gmail.com>
2023-01-10 12:38:20 +0100

    maj

--- col-img-txt/app.js
+++ col-img-txt/app.js
@@ -1,11 +1,11 @@
 $(window).scroll((e)=>{ 
     if ($(this).scrollTop() + 250) {console.log(e)
-        $(".bs-grid-text-contain").css({"transition" : "all 2s",  "color": "blue","margin-top": "150px"})
+        $(".bs-grid-text-contain").css({"transition" : "all 2s", "margin-top": "150px"})
       }
       var docTop = $(document).scrollTop() +50,
             anchorTop = $(".bs-grid-text-contain").offset().top;
       if (docTop > anchorTop) {
-        $(".bs-grid-text-contain").css({"transition" : "all 2s",  "color": "red","margin-top": "-150px"})
+        $(".bs-grid-text-contain").css({"transition" : "all 2s", "margin-top": "-150px"})
 
       }
 })
